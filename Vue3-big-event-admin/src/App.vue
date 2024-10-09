8554f9253585513b384ae7aa9c60c16b3e3ad045
AliothElish <151227961+AliothElish@users.noreply.github.com>
2024-10-09 15:00:59 +0800

    element plus

--- Vue3-big-event-admin/src/App.vue
+++ Vue3-big-event-admin/src/App.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push('/list')
+}
+</script>
 
 <template>
-  <div>I am App</div>
+  <div>
+    I am App
+    <button @click="$router.push('/home')">to home</button>
+    <button @click="goList">to list</button>
+  </div>
 </template>
 
 <style scoped></style>
