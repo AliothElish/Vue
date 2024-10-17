78e7e02ac304d2c8f17b0989cc4e7a25ea0da88f
AliothElish <151227961+AliothElish@users.noreply.github.com>
2024-10-17 17:24:03 +0800

    Update

--- Vue3-big-event-admin/src/stores/modules/counter.js
+++ Vue3-big-event-admin/src/stores/modules/counter.js
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
+// 数字计数器模块
 export const useCountStore = defineStore('big-count', () => {
   const count = ref(100)
-  const add = n => {
+  const add = (n) => {
     count.value += n
   }
   return {
